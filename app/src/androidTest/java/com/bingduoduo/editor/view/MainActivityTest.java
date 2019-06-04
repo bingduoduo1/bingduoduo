bcb2d4d1c1f62ecd336e9448a00234784f020719
binggge <15231112@buaa.edu.cn>
2019-06-04 21:21:52 +0800

    instrumentTest with bugs to fix

--- app/src/androidTest/java/com/bingduoduo/editor/view/MainActivityTest.java
+++ app/src/androidTest/java/com/bingduoduo/editor/view/MainActivityTest.java
@@ -11,6 +11,7 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.*;
 
 public class MainActivityTest {
