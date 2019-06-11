0841305324f1ff5b779bf8c3145067bde8bfd5f3
PaParaZz1 <niuyazhe@buaa.edu.cn>
2019-06-12 02:35:09 +0800

    fix scp password bug

--- app/src/main/java/model/application/auto_train/server_tool/ScpTool.java
+++ app/src/main/java/model/application/auto_train/server_tool/ScpTool.java
@@ -1,39 +1,73 @@
 package model.application.auto_train.server_tool;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.OutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
-public class ScpTool extends ServerTool{
+public class ScpTool extends ServerTool {
     private static ScpTool mScpTool = new ScpTool();
     private String mExecRecord;
 
-    public String getExecRecord() {
-        return mExecRecord;
-    }
     private ScpTool() {
         super.setTempFileName("scp_temp_file.txt");
         mExecRecord = null;
     }
+
     public static ScpTool createScpTool() {
         return mScpTool;
     }
+
+    public String getExecRecord() {
+        return mExecRecord;
+    }
+
     @Override
     public void sendMessageByPath(String localPath, String serverPath) {
-        String shell_cmd = "scp " + localPath + " nyz@10.135.250.200:" + serverPath + "\n";
+        String shell_cmd = "sshpass -p xxx scp " + localPath + " nyz@10.136.47.63:" + serverPath + "\n";
+        System.out.println(shell_cmd);
         this.scpExec(shell_cmd);
     }
 
     @Override
     public void receiveMessageByPath(String localPath, String serverPath) {
-        String shell_cmd = "scp " + " nyz@10.135.250.200:" + serverPath + " " + localPath + "\n";
+        String shell_cmd = "sshpass -p xxx scp " + " nyz@10.136.47.63:" + serverPath + " " + localPath + "\n";
         this.scpExec(shell_cmd);
     }
 
     private void scpExec(String cmd) {
         try {
             Process scp_process = Runtime.getRuntime().exec(cmd);
-            OutputStream out = scp_process.getOutputStream();
-            mExecRecord = out.toString();
+
+            InputStream in = scp_process.getInputStream();
+            InputStream err = scp_process.getErrorStream();
+
+            try {
+                int ret = scp_process.waitFor();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            BufferedReader reader_in = new BufferedReader(new InputStreamReader(in));
+            StringBuilder builder_in = new StringBuilder();
+            while (true) {
+                String temp = reader_in.readLine();
+                if (temp == null) {
+                    break;
+                } else {
+                    builder_in.append(temp + "\n");
+                }
+            }
+            BufferedReader reader_err = new BufferedReader(new InputStreamReader(err));
+            StringBuilder builder_err = new StringBuilder();
+            while (true) {
+                String temp = reader_err.readLine();
+                if (temp == null) {
+                    break;
+                } else {
+                    builder_err.append(temp + "\n");
+                }
+            }
+            mExecRecord = "stdout:\n" + builder_in.toString() + "stderr:\n" + builder_err.toString();
         } catch (IOException e) {
             e.printStackTrace();
         }
