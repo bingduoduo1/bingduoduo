be23682dc3062bfcab5f66b36f645a530f53dd84
binggge <15231112@buaa.edu.cn>
2019-06-12 19:46:06 +0800

    add test for server_tool

---
+++ app/src/test/java/model/application/auto_train/server_tool/ScpToolTest.java
@@ -0,0 +1,24 @@
+package model.application.auto_train.server_tool;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ScpToolTest {
+
+    @Test
+    public void createScpTool() {
+    }
+
+    @Test
+    public void getExecRecord() {
+    }
+
+    @Test
+    public void sendMessageByPath() {
+    }
+
+    @Test
+    public void receiveMessageByPath() {
+    }
+}
